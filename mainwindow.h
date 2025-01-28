83e528cc1b228a2efd8e3c562a3794305e08f4e6
iRessqq <iress10@mail.ru>
2025-01-28 23:41:09 +0300

    project

---
+++ mainwindow.h
@@ -0,0 +1,26 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QWidget>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QMessageBox>
+
+class MainWindow : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+
+private:
+    QLineEdit *inputAction;
+
+    bool connectToDb();
+    void saveToDb();
+    void showAllActions();
+};
+
+#endif // MAINWINDOW_H
