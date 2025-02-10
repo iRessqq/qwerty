40da0d111e16b83c1e367809a52c7ca01aadec5d
iRessqq <iress10@mail.ru>
2025-02-10 21:00:41 +0300

    docs: добавлена система документирования doxygen

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,8 @@
+/**
+ * @file mainwindow.cpp
+ * @brief реализация класса MainWindow
+ */
+
 #include "mainwindow.h"
 
 #include <QVBoxLayout>
@@ -12,30 +17,44 @@
 #include <QSqlTableModel>
 #include <QHeaderView>
 
+/**
+ * @brief конструктор MainWindow, создает интерфейс
+ * @param parent родительский виджет
+ */
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent) {
 
+    // проверка подключения к бд
     if (!connectToDb()) {
         QMessageBox::critical(this, "ошибка", "не подключен к бд");
         exit(1);
     }
 
+    // вертикальный layout
     QVBoxLayout *layout = new QVBoxLayout(this);
 
+    // поле для ввода
     inputAction = new QLineEdit(this);
     layout->addWidget(inputAction);
 
+    // кнопка сохранения
     QPushButton *saveButton = new QPushButton("сохранить", this);
     layout->addWidget(saveButton);
     connect(saveButton, &QPushButton::clicked, this, &MainWindow::saveToDb);
 
+    // таблица для отображения данных
     tableView = new QTableView(this);
     layout->addWidget(tableView);
 
+    // настройка модели и заполнение таблицы
     setupTableModel();
     showAllActions();
 }
 
+/**
+ * @brief подключение к бд
+ * @return true - успешно, false - error
+ */
 bool MainWindow::connectToDb() {
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("log.db");
@@ -44,11 +63,15 @@ bool MainWindow::connectToDb() {
         return false;
     }
 
+    //создание таблицы, если ее нет
     createTableIfNotExist();
 
     return true;
 }
 
+/**
+* @brief создание таблицы, если ее не существует
+ */
 void MainWindow::createTableIfNotExist() {
     QSqlQuery query;
     if (!query.exec("CREATE TABLE IF NOT EXISTS actions ("
@@ -59,6 +82,9 @@ void MainWindow::createTableIfNotExist() {
     }
 }
 
+/**
+ * @brief сохранение действия в бд
+ */
 void MainWindow::saveToDb() {
     QString actionText = inputAction->text();
 
@@ -79,10 +105,16 @@ void MainWindow::saveToDb() {
     }
 }
 
+/**
+ * @brief обновление данных в таблице
+ */
 void MainWindow::showAllActions() {
     model->select();
 }
 
+/**
+ * @brief настраивает модель таблицы
+ */
 void MainWindow::setupTableModel() {
     model = new QSqlTableModel(this);
     model->setTable("actions");
